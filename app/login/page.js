e0808a9f3a7d6213916bdd0e146666f4ef13da9e
darjopet-boop <darjopet@gmail.com>
2026-08-18 12:32:54 +0100

    fix: remove duplicate useState import in login page

--- app/login/page.js
+++ app/login/page.js
@@ -1,9 +1,6 @@
 "use client";
 
-"use client";
-
 import { useState, useEffect } from "react";
-import { useState } from "react";
 import { createClient } from "@/lib/supabase";
 import { useRouter } from "next/navigation";
 
